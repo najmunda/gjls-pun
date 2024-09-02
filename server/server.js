ea01a12bf3dcc4db67fd626f20738a4ac5aa5d5e
najmunda <najmundaakmal@gmail.com>
2024-09-02 13:05:12 +0700

    feat: Init backend
    Move front and back code to respective folder

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import episodes from "./routes/episodes.js"
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/episodes", episodes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
